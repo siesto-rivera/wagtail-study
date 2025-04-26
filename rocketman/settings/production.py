e9fdf7375046a807e04437aa546cd7ec893a08c1
siesto <siesto.rivera@gmail.com>
2025-04-26 21:46:48 +0900

    배포 사전준비

--- rocketman/settings/production.py
+++ rocketman/settings/production.py
@@ -3,6 +3,24 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = "nmdrm)x)*@w!mt!6lm+5b7w!fma$y4gs6cjc1d3sp81(58i*vc"
+ALLOWED_HOSTS = [
+    "localhost",
+    "wagtailsite.peoplepower21.org",
+    "*",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "siesto",
+        "USER": "siesto",
+        "PASSWORD": "wagtaildb",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
 cwd = os.getcwd()
 CACHES = {
     "default": {
@@ -11,7 +29,14 @@ CACHES = {
     }
 }
 
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://d62379f1a632e05a4141ef0084331738@o521974.ingest.us.sentry.io/4509219624714240",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 try:
     from .local import *
 except ImportError:
