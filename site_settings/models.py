d13d115561251a33464004477abe825fe82d35c3
siesto <siesto.rivera@gmail.com>
2025-04-26 14:19:23 +0900

    global site settings - social media setting - model

---
+++ site_settings/models.py
@@ -0,0 +1,27 @@
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
+from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.contrib.settings.models import BaseSiteSetting
+from wagtail.contrib.settings.registry import register_setting
+
+
+@register_setting
+class SocialMediaSettings(BaseSiteSetting):
+
+    facebook = models.URLField(blank=True, help_text="Enter your Facebook URL")
+    twitter = models.URLField(blank=True, help_text="Enter your Twitter URL")
+    youtube = models.URLField(blank=True, help_text="Enter your YouTube URL")
+    instagram = models.URLField(blank=True, help_text="Enter your Instagram URL")
+
+    panels = [
+        FieldPanel("facebook"),
+        FieldPanel("twitter"),
+        FieldPanel("youtube"),
+        FieldPanel("instagram"),
+    ]
+
+    def save(self, *args, **kwargs):
+        key = make_template_fragment_key("footer_social_settings")
+        cache.delete(key)
+        return super().save(*args, **kwargs)
